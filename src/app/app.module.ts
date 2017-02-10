90ea84e100e1c669d3ce2797b16596f965277a49
Fabricio Pavano <faripavano@gmail.com>
2017-02-10 12:33:30 -0300

    formulario terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TicketFormComponent } from './ticket-form/ticket-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TicketFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ticket.ts
@@ -0,0 +1,8 @@
+export class Ticket {
+  constructor(
+    public id: number,
+    public title: string,
+    public category: string,
+    public description: string,
+  ) {  }
+}
